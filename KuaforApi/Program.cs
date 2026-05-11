28014262359a9cc6b37c4defe63fe959ca6058a4
zeynep zahide <154972595+Zeynepzahide@users.noreply.github.com>
2026-05-11 16:10:49 +0300

    z
    z

--- KuaforApi/Program.cs
+++ KuaforApi/Program.cs
@@ -8,8 +8,12 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString =
+    Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<AuthService>();
 
