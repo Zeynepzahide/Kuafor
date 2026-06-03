99b3fa80b82a2746c5ba24b2b204cd6a6736864f
zeynep zahide <154972595+Zeynepzahide@users.noreply.github.com>
2026-06-03 13:16:46 +0300

    .

--- kuafor_app/android/app/build.gradle.kts
+++ kuafor_app/android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.example.kuafor_app"
 
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
